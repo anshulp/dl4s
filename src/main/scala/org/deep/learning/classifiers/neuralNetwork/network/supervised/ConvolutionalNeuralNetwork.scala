a5419b2d875637150eef0035a1690deeceb397a5
Anshul Pandey (anpandey) <anpandey@expedia.com>
2016-12-05 23:48:07 -0800

    add supervised and unsupervised structure

---
+++ src/main/scala/org/deep/learning/classifiers/neuralNetwork/network/supervised/ConvolutionalNeuralNetwork.scala
@@ -0,0 +1,7 @@
+package org.deep.learning.classifiers.neuralNetwork.network.supervised
+
+import org.deep.learning.classifiers.neuralNetwork.network.NeuralNetwork
+
+class ConvolutionalNeuralNetwork extends NeuralNetwork {
+
+}
---
+++ src/main/scala/org/deep/learning/classifiers/neuralNetwork/network/unsupervised/Autoencoder.scala
@@ -0,0 +1,7 @@
+package org.deep.learning.classifiers.neuralNetwork.network.unsupervised
+
+import org.deep.learning.classifiers.neuralNetwork.network.NeuralNetwork
+
+class Autoencoder extends NeuralNetwork{
+
+}
