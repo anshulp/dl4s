2dd18a05fba913271bbe2d09c571891d7119b7a4
Anshul Pandey (anpandey) <anpandey@expedia.com>
2016-12-05 23:52:45 -0800

    add tanh activation

---
+++ src/main/scala/org/deep/learning/classifiers/neuralNetwork/activation/HyperbolicTanFunction.scala
@@ -0,0 +1,13 @@
+package org.deep.learning.classifiers.neuralNetwork.activation
+
+class HyperbolicTanFunction extends ActivationFunction {
+
+  def activate(value: BigDecimal): BigDecimal = {
+    0
+  }
+
+  def derivative(value: BigDecimal): BigDecimal = {
+    0
+  }
+
+}
